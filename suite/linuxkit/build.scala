fb0dfb265798a234d1a47c7129e393be3699eb0e
Alexander Slesarev <alex.slesarev@gmail.com>
2017-10-11 09:29:26 +1000

    Scalasti templating usage has been added.

--- suite/linuxkit/build.scala
+++ suite/linuxkit/build.scala
@@ -4,9 +4,12 @@
 scalaVersion := "2.12.3"
 scalacOptions ++= Seq("-deprecation", "-feature")
 libraryDependencies += "com.github.scopt" %% "scopt" % "3.7.0"
+libraryDependencies += "org.clapper" %% "scalasti" % "3.0.1"
 */
 
 import java.io._
+import org.clapper.scalasti.ST
+import scala.util.{Success, Failure}
 import sys.process._
 
 case class Config(
@@ -28,33 +31,20 @@ val parser = new scopt.OptionParser[Config]("run.scala") {
     c.copy(usb = true) ).text("generate USB bootable image")
 }
 
-val DhcpcdLocal = """
-  - name: dhcpcd
-    image: linuxkit/dhcpcd:d4408777ed6b6e6e562a5d4938fd09804324b33e
-    command: ["/sbin/dhcpcd", "--nobackground", "-f", "/dhcpcd.conf", "-1"]
-"""
-val DhcpcdWlan0 = """
-  - name: dhcpcd
-    image: linuxkit/dhcpcd:d4408777ed6b6e6e562a5d4938fd09804324b33e
-    command: ["/sbin/dhcpcd", "wlan0"]
-"""
-
 def entryPoint(args: Array[String]): Unit = {
   parser.parse(args, Config()) match {
     case Some(config) => {
       new File("build").mkdir()
-      val root = if (config.usb) "root=/dev/sda1" else ""
-      val dhcpcdLocal = if (config.usb) "" else DhcpcdLocal
-      val dhcpcdWlan0 = if (config.usb) DhcpcdWlan0 else ""
       val src = scala.io.Source.fromFile("linuxkit.yml.in").mkString
-      val dst = src.replace("%%ROOT%%", root).
-        replace("%%SSID%%", config.ssid).
-        replace("%%PASSWORD%%", config.password).
-        replace("%%DHCPCD-LOCAL%%", dhcpcdLocal).
-        replace("%%DHCPCD-WLAN0%%", dhcpcdWlan0)
-      new PrintWriter("build/linuxkit.yml") { write(dst); close }
-      "docker build . -t swb".!
-      "moby build -format iso-bios -dir build build/linuxkit.yml".!
+      val template = ST(src).add("config", config)
+      template.render() match {
+        case Success(dst) => {
+          new PrintWriter("build/linuxkit.yml") { write(dst); close }
+          "docker build . -t swb".!
+          "moby build -format iso-bios -dir build build/linuxkit.yml".!
+        }
+        case Failure(ex) => println(ex)
+      }
     }
     case None =>
     // arguments are bad, error message will have been displayed
