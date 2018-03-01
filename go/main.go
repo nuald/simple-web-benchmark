309420b121bc6dfa7e5fa8e4c200707c297fbcad
Alexander Slesarev <alex.slesarev@gmail.com>
2018-03-01 09:49:37 -0800

    Added --port flag into the Go app.

--- go/main.go
+++ go/main.go
@@ -1,26 +1,34 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
-  "regexp"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
 )
 
 func main() {
-  fmt.Printf("Master %d is running\n", os.Getpid())
-  reg := regexp.MustCompile("^/greeting/([a-z]+)$")
-  http.ListenAndServe("127.0.0.1:3000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    switch r.URL.Path {
-      case "/":
-        fmt.Fprint(w, "Hello World!")
-      default:
-        matches := reg.FindStringSubmatch(r.URL.Path)
-        if matches != nil {
-          fmt.Fprintf(w, "Hello, %s", matches[1])
-        } else {
-          http.NotFound(w, r)
-        }
-    }
-  }))
+	port := flag.Int("port", 3000, "server port")
+	flag.Parse()
+	fmt.Printf("Master %d is running on port %d\n", os.Getpid(), *port)
+	reg := regexp.MustCompile("^/greeting/([a-z]+)$")
+	host := fmt.Sprintf("127.0.0.1:%d", *port)
+	err := http.ListenAndServe(host, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/":
+			fmt.Fprint(w, "Hello World!")
+		default:
+			matches := reg.FindStringSubmatch(r.URL.Path)
+			if matches != nil {
+				fmt.Fprintf(w, "Hello, %s", matches[1])
+			} else {
+				http.NotFound(w, r)
+			}
+		}
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
