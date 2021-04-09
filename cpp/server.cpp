9fa9bd0d11f1cb4192fa0441bb7763ca48fce648
Alexander Slesarev <alex.slesarev@gmail.com>
2021-04-08 22:25:00 -0600

    Tried sync beast.

--- cpp/server.cpp
+++ cpp/server.cpp
@@ -1,190 +1,179 @@
-
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/asio.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/config.hpp>
 #include <boost/format.hpp>
-#include <chrono>
 #include <cstdlib>
-#include <cstring>
 #include <iostream>
-#include <list>
 #include <memory>
 #include <string>
 #include <thread>
 #include <unistd.h>
 #include <fstream>
+#include <regex>
 
-namespace beast = boost::beast;
-namespace http = beast::http;
-namespace net = boost::asio;
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 using format = boost::format;
-using tcp = boost::asio::ip::tcp;
 
-class http_worker {
+std::regex reg("/greeting/([a-z]+)");
 
-public:
-  http_worker(http_worker const&) = delete;
-  http_worker& operator=(http_worker const&) = delete;
-
-  http_worker(tcp::acceptor& acceptor): acceptor_(acceptor) {}
-
-  void start() {
-    accept();
-    check_deadline();
-  }
-
-private:
-  using request_body_t = http::string_body;
-
-    // The acceptor used to listen for incoming connections.
-  tcp::acceptor& acceptor_;
-
-    // The socket for the currently connected client.
-  tcp::socket socket_{acceptor_.get_executor()};
-
-    // The buffer for performing reads
-  beast::flat_static_buffer<8192> buffer_;
-
-    // The parser for reading the requests
-  boost::optional<http::request_parser<request_body_t>> parser_;
-
-    // The timer putting a time limit on requests.
-  net::steady_timer request_deadline_{
-    acceptor_.get_executor(), (std::chrono::steady_clock::time_point::max)()
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
+template<class Body, class Allocator, class Send>
+void handle_request(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+  // Returns a bad request response
+  auto const bad_request = [&req](beast::string_view why) {
+    http::response<http::string_body> res{http::status::bad_request, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/plain");
+    res.keep_alive(req.keep_alive());
+    res.body() = std::string(why);
+    res.prepare_payload();
+    return res;
   };
 
-  // The string-based response message.
-  boost::optional<http::response<http::string_body>> string_response_;
+  // Returns a not found response
+  auto const not_found = [&req](const std::string& message) {
+    http::response<http::string_body> res{http::status::not_found, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/plain");
+    res.keep_alive(req.keep_alive());
+    res.body() = message;
+    res.prepare_payload();
+    return res;
+  };
 
-  // The string-based response serializer.
-  boost::optional<http::response_serializer<http::string_body>> string_serializer_;
+  // Returns HTTP OK response with the msesage
+  auto const ok = [&req](const std::string& message) {
+    http::response<http::string_body> res{http::status::ok, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/plain");
+    res.keep_alive(req.keep_alive());
+    res.body() = message;
+    res.prepare_payload();
+    return res;
+  };
 
-  void accept() {
-      // Clean up any previous connection.
-    beast::error_code ec;
-    socket_.close(ec);
-    buffer_.consume(buffer_.size());
-
-    acceptor_.async_accept(
-      socket_,
-      [this](beast::error_code ec) {
-        if (ec) {
-          accept();
-        } else {
-          // Request must be fully processed within 60 seconds.
-          request_deadline_.expires_after(std::chrono::seconds(60));
-          read_request();
-        }
-      });
+  // Make sure we can handle the method
+  if (req.method() != http::verb::get) {
+      return send(bad_request("Unknown HTTP-method"));
   }
 
-  void read_request() {
-    // On each read the parser needs to be destroyed and
-    // recreated. We store it in a boost::optional to
-    // achieve that.
-    //
-    // Arguments passed to the parser constructor are
-    // forwarded to the message object. A single argument
-    // is forwarded to the body constructor.
-    //
-    // We construct the dynamic body with a 1MB limit
-    // to prevent vulnerability to buffer attacks.
-    //
-    parser_.emplace(std::piecewise_construct, std::make_tuple());
-
-    http::async_read(
-      socket_,
-      buffer_,
-      *parser_,
-      [this](beast::error_code ec, std::size_t) {
-        if (ec) {
-          accept();
-        } else {
-          process_request(parser_->get());
-        }
-      });
+  std::string path(req.target());
+  std::smatch match;
+  if (path == "/") {
+    return send(ok("Hello world!"));
+  } else if (std::regex_match(path, match, reg)) {
+    return send(ok(str(format("Hello, %s") % match[1].str())));
   }
+  return send(not_found("Not Found"));
+}
 
-  void process_request(http::request<request_body_t> const& req) {
-    switch (req.method()) {
-      case http::verb::get:
-      send_response(http::status::ok, "Hello World!");
-      break;
+//------------------------------------------------------------------------------
 
-      default:
-      // We return responses indicating an error if
-      // we do not recognize the request method.
-      send_response(
-        http::status::bad_request,
-        "Invalid request-method '" + std::string(req.method_string()) + "'\r\n");
-      break;
-    }
-  }
+// Report a failure
+void fail(beast::error_code ec, char const* what) {
+  std::cerr << what << ": " << ec.message() << "\n";
+}
 
-  void send_response(http::status status, std::string const& body) {
-    string_response_.emplace(std::piecewise_construct, std::make_tuple());
+// This is the C++11 equivalent of a generic lambda.
+// The function object is used to send an HTTP message.
+template<class Stream> struct send_lambda {
+  Stream& stream_;
+  bool& close_;
+  beast::error_code& ec_;
 
-    string_response_->result(status);
-    string_response_->keep_alive(false);
-    string_response_->set(http::field::server, "Beast");
-    string_response_->set(http::field::content_type, "text/plain");
-    string_response_->body() = body;
-    string_response_->prepare_payload();
+  explicit send_lambda(Stream& stream, bool& close, beast::error_code& ec)
+    : stream_(stream), close_(close), ec_(ec) {}
 
-    string_serializer_.emplace(*string_response_);
+  template<bool isRequest, class Body, class Fields>
+  void operator()(http::message<isRequest, Body, Fields>&& msg) const {
+    // Determine if we should close the connection after
+    close_ = msg.need_eof();
 
-    http::async_write(
-      socket_,
-      *string_serializer_,
-      [this](beast::error_code ec, std::size_t) {
-        socket_.shutdown(tcp::socket::shutdown_send, ec);
-        string_serializer_.reset();
-        string_response_.reset();
-        accept();
-      });
-  }
-
-  void check_deadline() {
-    // The deadline may have moved, so check it has really passed.
-    if (request_deadline_.expiry() <= std::chrono::steady_clock::now()) {
-      // Close socket to cancel any outstanding operation.
-      beast::error_code ec;
-      socket_.close();
-
-      // Sleep indefinitely until we're given a new deadline.
-      request_deadline_.expires_at(std::chrono::steady_clock::time_point::max());
-    }
-
-    request_deadline_.async_wait(
-      [this](beast::error_code) {
-        check_deadline();
-      });
+    // We need the serializer here because the serializer requires
+    // a non-const file_body, and the message oriented version of
+    // http::write only works with const messages.
+    http::serializer<isRequest, Body, Fields> sr{msg};
+    http::write(stream_, sr, ec_);
   }
 };
+
+// Handles an HTTP server connection
+void do_session(tcp::socket& socket) {
+  bool close = false;
+  beast::error_code ec;
+
+  // This buffer is required to persist across reads
+  beast::flat_buffer buffer;
+
+  // This lambda is used to send messages
+  send_lambda<tcp::socket> lambda{socket, close, ec};
+
+  for (;;) {
+    // Read a request
+    http::request<http::string_body> req;
+    http::read(socket, buffer, req, ec);
+    if (ec == http::error::end_of_stream) {
+      break;
+    }
+    if (ec) {
+      return fail(ec, "read");
+    }
+
+    // Send the response
+    handle_request(std::move(req), lambda);
+    if (ec) {
+      return fail(ec, "write");
+    }
+
+    if (close) {
+      // This means we should close the connection, usually because
+      // the response indicated the "Connection: close" semantic.
+      break;
+    }
+  }
+
+  // Send a TCP shutdown
+  socket.shutdown(tcp::socket::shutdown_send, ec);
+
+  // At this point the connection is closed gracefully
+}
+
+//------------------------------------------------------------------------------
 
 int main() {
   try {
     auto const address = net::ip::make_address("0.0.0.0");
-    unsigned short port = 3000;
-    int num_workers = std::thread::hardware_concurrency();
-    auto pid = getpid();
+    auto const port = 3000;
+    auto const pid = getpid();
     {
       std::ofstream pid_file(".pid");
       pid_file << pid;
     }
     std::cout << format("Master %d is running on port %d\n") % pid % port;
 
+    // The io_context is required for all I/O
     net::io_context ioc{1};
+
+    // The acceptor receives incoming connections
     tcp::acceptor acceptor{ioc, {address, port}};
+    for (;;) {
+        // This will receive the new connection
+      tcp::socket socket{ioc};
 
-    std::list<http_worker> workers;
-    for (int i = 0; i < num_workers; ++i) {
-      workers.emplace_back(acceptor);
-      workers.back().start();
+        // Block until we get a connection
+      acceptor.accept(socket);
+
+        // Launch the session, transferring ownership of the socket
+      std::thread{std::bind(&do_session, std::move(socket))}.detach();
     }
-
-    ioc.run();
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return EXIT_FAILURE;
