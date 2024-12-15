dacc8226f733516688cd4a5db510e9168f25401f
Sergei Giniatulin <kornburn@gmail.com>
2024-12-16 01:44:24 +0200

    [D] serverino
    Added serverino example

---
+++ d/source/app.d
@@ -0,0 +1,44 @@
+module app;
+
+import serverino;
+import std.getopt;
+import std.process: thisProcessID;
+import std.regex: ctRegex, matchFirst;
+import std.stdio: File, writeln;
+import std.logger: LogLevel;
+
+auto ctr= ctRegex!("/greeting/([a-z]+)");
+
+mixin ServerinoMain;
+ushort port;
+
+@onDaemonStart save_pid() {
+    auto f = File(".pid", "w+");
+    f.writeln(thisProcessID);
+    writeln(i"Master $(thisProcessID) is running on port $(port)");
+}
+
+@onServerInit ServerinoConfig configure(string[] args) {
+    auto info = getopt(args, "port", &port);
+
+    return ServerinoConfig
+        .create()
+        .enableKeepAlive()
+        .setLogLevel(LogLevel.critical)
+        .addListener("0.0.0.0", port)
+        .setWorkers(25);
+}
+
+@endpoint void hello(Request req, Output output) {
+    if (req.uri == "/")
+        output ~= "Hello world!";
+    else {
+        auto ch = req.uri.matchFirst(ctr);
+        if (!ch.empty)
+            output ~= ch[1];
+        else {
+            output.status = 404;
+            return;
+        }
+    }
+}
